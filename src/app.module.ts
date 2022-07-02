21875c18c3ddef55ab9a8e781d55281c751f1cbf
Rafal Zw <zwierz.rafal@gmail.com>
2022-07-02 22:56:21 +0200

    Added PostsModule, PostsController, PostsService.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database.module';
 import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
+  imports: [UsersModule, DatabaseModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
