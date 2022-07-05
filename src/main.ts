7182ce3bc3f08655300ae4727beee81b62322a7f
Rafal Zw <zwierz.rafal@gmail.com>
2022-07-05 23:28:36 +0200

    Exported types directory.

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: true,
       transform: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
