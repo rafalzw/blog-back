0e65821ab5aaf15534df1767c3eaddde8470889e
Rafal Zw <zwierz.rafal@gmail.com>
2022-07-10 13:53:49 +0200

    Added static folder with images.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as path from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
   app.setGlobalPrefix('api');
+  app.useStaticAssets(path.join(__dirname, '..', 'storage/post-photos'), {
+    prefix: '/api/post-photos',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: true,
       transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
   await app.listen(3001);
